dab3daf6d0cbad9c62a03ba5afba63bd0755de67
phanthehung263 <phanthehung263@gmail.com>
2023-03-19 18:40:58 +0700

    first commit

---
+++ Lý Thuyết/Javascript/app.js
@@ -0,0 +1,80 @@
+// console.log("welcome to javascript");
+// console.log("Hello");
+// console.log("This is test text");
+
+// console.error("Day la mot loi");
+// console.warn("Day la canh bao");
+
+// // tên biến bắt đầu bằng chữ cái, hoặc "_" , hoặc "$"
+// // trong đặt tên biến: có thể dùng số
+
+// let name; // khai báo biến
+// name = "Giá trị mới của name"; // gán giá trị cho biến
+// name = "Helllo, this is something";
+
+// console.log(name); // hiển thị giá trị của biến name lên màn hình
+
+// let age = 10; // khai báo & gán giá trị cho biến
+
+// const hello = "Cần lao vi tiên thủ";
+// hello = "hello";
+
+// console.log(hello)
+
+// kiểu số
+// let age = 21;
+// let weight = 63;
+// let height = 170;
+
+// // kiểu chuỗi (string)
+// let slogan = "có công mài sắt, có ngày nên kim";
+// let name = "Phan Thế Hùng";
+// let address = "Hà Nội";
+// let phonenumber = "0828125885";
+
+// // kiểu Boolean ()
+// let hasGirlfriend = false;
+// let isHandsome = true;
+// let isRich = false;
+
+// // kiểu null
+// let girlFriend = null;
+// let money = null;
+
+// // kiểu undefined
+// let future;
+// let wife;
+
+// console.log(future);
+
+// let a = 10;
+// let b = "10";
+// console.log(a==b); // toán tử so sánh cho ra 1 giá trị boolean
+// console.log(a===b);// bằng giá trị và kiểu dữ liệu
+
+// let x =10;
+// let y =20;
+// let z ="30";
+// let res1 = x + z;
+// console.log(res1);
+
+// let name1 = 'AAA';
+// let name2= 'aaa';
+// console.log(name1==name2)
+
+// let a =10;
+// let b = 20;
+// let c = 30;
+// let d ='40';
+// console.log(a+b+c+d);
+// console.log(d-(a*b)-c);
+// console.log(c+d);
+// console.log(d*d-b);
+
+let a = `tôi là hùng`
+let text = `hùng rất đẹp trai
+
+và người yêu hùng cũng rất xinh`;
+text.length
+console.log(text);
+
